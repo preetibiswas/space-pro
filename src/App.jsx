88c33ee6a4161f90c94a4fa076f1660d4871989c
Preeti biswas <preeti.biswas@brovitech.com>
2023-12-20 23:16:34 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import NavBar from "./Component/Navbar"
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <NavBar/>
+    </div>
+   
+  )
+}
+
+export default App
