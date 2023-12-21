1a21f3e0461f1fd163a2f8ae59495417056dcfd3
Preeti biswas <preeti.biswas@brovitech.com>
2023-12-22 00:30:51 +0530

    project-component-done

--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import React from "react";
+import { Col, Container, Nav, Row, Tab } from "react-bootstrap";
+import colorShap2 from "../assets/img/color-sharp2.png";
+import proImg1 from "../assets/img/project-img1.png";
+import proImg2 from "../assets/img/project-img2.png";
+import proImg3 from "../assets/img/project-img3.png";
+import ProjectCard from "./ProjectCard";
 
 const Project = () => {
-  return (
-    <div>Project</div>
-  )
-}
+  const project = [
+    {
+      title: "business Startup",
+      description: "Design & Development",
+      imgUrl: proImg1,
+    },
+    {
+        title: "business Startup",
+        description: "Design & Development",
+        imgUrl: proImg1,
+      },
+      {
+        title: "business Startup",
+        description: "Design & Development",
+        imgUrl: proImg2,
+      },
+      {
+        title: "business Startup",
+        description: "Design & Development",
+        imgUrl: proImg3,
+      },
+      {
+        title: "business Startup",
+        description: "Design & Development",
+        imgUrl: proImg1
+      },
+      {
+        title: "business Startup",
+        description: "Design & Development",
+        imgUrl: proImg2,
+      },
+  ];
+  return <section className="project" id="project">
+    <Container>
+        <Row>
+            <Col>
+            <h2>Projects</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere corrupti commodi dolore illo iure nesciunt eos, voluptatum numquam, sint quas voluptas, temporibus vel ipsa. Blanditiis ducimus quos est quidem excepturi.</p>
+          
+            <Tab.Container id="projects-tabs" defaultActiveKey='first'>
+            <Nav variant="pills"
+            defaultActiveKey='first'
 
-export default Project
+            className="nav-pills mb-5 justify-content align-item-center" id="pills-tab">
+            <Nav.Item>
+              <Nav.Link eventKey="first">Tab 1</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="second">Tab 2</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="Third">Tab 3</Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <Tab.Content>
+            <Tab.Pane eventKey='first'>
+                <Row>
+                    {project.map((pro,ind)=>{
+                        return (
+                            <ProjectCard key={ind} {...pro}/>
+                        )
+                    })}
+
+                </Row>
+
+            </Tab.Pane>
+          </Tab.Content>
+
+            </Tab.Container>
+            </Col>
+        </Row>
+    </Container>
+    <img src={colorShap2} alt="img" className="'background-image-right" />
+  </section>;
+};
+
+export default Project;
